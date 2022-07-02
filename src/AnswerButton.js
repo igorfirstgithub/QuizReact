ac70a21fe65d29e0abfe6f8d773da26b7379f846
Igor <bigbookreader@gmail.com>
2022-07-02 17:11:19 +0300

    Logic removal

--- src/AnswerButton.js
+++ src/AnswerButton.js
@@ -1,7 +1,12 @@
 import React from "react"
 
 export default function AnswerButton(props) {
-    return(<div className="rectangle">
-    {props.value.val}
-    </div>)
+    
+    return(
+        
+        <div className="rectangle" onClick={props.chooseAns}>
+            {props.value.val}
+        </div>          
+    
+    )
 }
--- src/QuizPage.js
+++ src/QuizPage.js
@@ -3,30 +3,45 @@ import AnswerButton from "./AnswerButton"
 
 export default function QuizPage(props) {
     
+    //Combining all answers together - both correct and incorrect [+ - - -]
     const answerArray = [props.value.correct_answer, ...props.value.incorrect_answers];
-    //console.log(props);
-    //console.log(answerArray);
+    
+    //Convert each answer in an object with additional properties "true" and "chosen"
     const ansObjects = answerArray.map((el, index) => {
         if (index === 0) {
-            return {val: el, correct: true}
+            return {val: el, correct: true, chosen: false}
         } else {
-            return {val: el, correct: false}
+            return {val: el, correct: false, chosen: false}
         }
     });
-    console.log(ansObjects);
-   // const buttonArrays = answerArray.map(el => <Answer is />)
-    let ansTogether = '';
-    for (let i = 0; i < ansObjects.length; i++) {
-        ansTogether += ansObjects[i].val + ' ';
+    
+    // Remove the correct answer from the beginning of the array to some random place
+    let randomPos = Math.floor(Math.random() * 4);
+    let temp = ansObjects[randomPos];
+    ansObjects[randomPos] = ansObjects[0];
+    ansObjects[0] = temp;
+    
+    // Answer onClick Function - set "chosen" to true   
+    function chooseAns(index) {
+       console.log(index) 
     }
     
-    const arrButtons = ansObjects.map((el, index) => <AnswerButton key={index} value={el} />)
+    // Form an array of answer buttons
+    const arrButtons = ansObjects.map((el, index) => <AnswerButton 
+    
+        key={index} 
+        value={el} 
+        chooseAns={() => chooseAns(index)}
+    
+    />)
+    
+    
     
     return(
-    
-    <div>
-        <p>{props.value.question}</p>
-        {arrButtons}
-        <hr />
-    </div>)
+        <div>
+            <p>{props.value.question}</p>
+            {arrButtons}
+            <hr />
+        </div>
+    )
 }
