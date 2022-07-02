b8290bbfad16ce42b8ad2f4ed7afe951579dedce
igorfirstgithub <bigbookreader@gmail.com>
2022-07-02 17:02:52 +0300

    List of questions added (#1)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import QuizPage from "./QuizPage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React now
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    
+    const startArray = [];
+    const [selectPage, setSelectPage] = React.useState(false);
+    const [quizData, setQuizData] = React.useState([]);
+        
+    for (let i = 0; i < 3; i++) {
+        startArray.push(i+1);
+    }
+    
+    const quizArray = quizData.map( (el, index) => <QuizPage key={index} value={el}  /> )
+        
+    function changePage() {
+        setSelectPage(prevPage => !prevPage);
+        
+        fetch("https://opentdb.com/api.php?amount=3&category=19&difficulty=easy&type=multiple").
+        then(res => res.json()).
+        then(res => setQuizData(res.results));
+        
+        
+    }
+    
+    
+    if (!selectPage) { // Start page
+        return (
+            <div>
+                <h1>Qizzical</h1>
+                <p>Some description</p>
+                <button type="button" onClick={changePage}>Start quiz</button>
+            </div>
+            )
+    } else {            // Quiz page
+        return (
+            <div>
+                {quizArray}
+                <button type="button">Submit answers</button>
+            </div>
+        )
+    }
 }
-
-export default App;
