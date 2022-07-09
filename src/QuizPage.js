cad4ea8b7b2aaf7e0d239cc5896d3a99eadbb21d
Igor <bigbookreader@gmail.com>
2022-07-09 09:08:19 +0300

    cycled quiz, included api

--- src/QuizPage.js
+++ src/QuizPage.js
@@ -2,36 +2,30 @@ import React from "react"
 import AnswerButton from "./AnswerButton"
 
 export default function QuizPage(props) {
+    //props now is an object with key and value properties
+    // value is an object:
+    //                        {question: string,
+    //                        answers: [{answer: string,
+    //                                   correct: boolean,
+    //                                   chosen: boolean                                  
+    //                                  }, {}, {}, {}]
     
-    //Combining all answers together - both correct and incorrect [+ - - -]
-    const answerArray = [props.value.correct_answer, ...props.value.incorrect_answers];
-    
-    //Convert each answer in an object with additional properties "true" and "chosen"
-    const ansObjects = answerArray.map((el, index) => {
-        if (index === 0) {
-            return {val: el, correct: true, chosen: false}
-        } else {
-            return {val: el, correct: false, chosen: false}
-        }
-    });
-    
-    // Remove the correct answer from the beginning of the array to some random place
-    let randomPos = Math.floor(Math.random() * 4);
-    let temp = ansObjects[randomPos];
-    ansObjects[randomPos] = ansObjects[0];
-    ansObjects[0] = temp;
-    
+      
+    //console.log(props.value)  ;
+      
     // Answer onClick Function - set "chosen" to true   
-    function chooseAns(index) {
-       console.log(index) 
+    function chooseAns(ansIndex) {
+       //console.log(ansIndex) 
+       props.chooseQuest(ansIndex, props.questIndex)
     }
     
     // Form an array of answer buttons
-    const arrButtons = ansObjects.map((el, index) => <AnswerButton 
-    
+    const arrButtons = props.value.answers.map((el, index) => <AnswerButton 
+    //Here every el is an object with answer {ans, corr, chosen}
         key={index} 
         value={el} 
-        chooseAns={() => chooseAns(index)}
+        chooseAns={() => chooseAns(index)} 
+        modeCheck={props.modeCheck} 
     
     />)
     
